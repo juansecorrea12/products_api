3194c60e6935c8dcdd432a0fb818f7707c8c8631
juan correa <juansecorrea12@gmail.com>
2021-05-06 21:43:44 -0500

    views

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const express = require('express');
 const body_parser = require('body-parser');
 const app = express();
 const api = require('./routes');
+const hbs = require('express-handlebars');
 
 app.use(body_parser.urlencoded({extended: false}));
 app.use(body_parser.json());
+app.engine('.hbs', hbs({
+    defaultLayout: 'default',
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 app.use('/api', api);
+app.get('/login', (req, res) => {
+    res.render('login')
+})
 
 
 module.exports = app;
